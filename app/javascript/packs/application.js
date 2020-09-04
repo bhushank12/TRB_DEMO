6e19ea525a7c4afaf40f6b9f3dd0e7296eb3fef1
Bhushan Kalode <bhushan.kalode@joshsoftware.com>
2020-09-04 13:57:10 +0530

    + add javascript in js file & apply style for datatable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("call")
 
 import $ from 'jquery';
 global.$ = jQuery;
