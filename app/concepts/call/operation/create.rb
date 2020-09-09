b545abd32d1c4ba9e9ed82dea90d8868364ccb60
Bhushan Kalode <bhushan.kalode@joshsoftware.com>
2020-09-09 21:56:50 +0530

    +add errors while creating call

--- app/concepts/call/operation/create.rb
+++ app/concepts/call/operation/create.rb
@@ -1,8 +1,13 @@
 module Call::Operation
   class Create < Trailblazer::Operation
     class Present < Trailblazer::Operation
+      pass :populate_errors
       step :build_model
       step Contract::Build(constant: Call::Contract::Form)
+
+      def populate_errors(ctx, **)
+        ctx[:errors] = []
+      end
 
       def build_model(ctx, **)
         ctx[:model] = Call.new
@@ -14,9 +19,16 @@ module Call::Operation
     step Contract::Validate(key: :call)
     step :prepopulate
     step Contract::Persist()
+    fail :log_errors
 
     def prepopulate(ctx, current_user:, **)
       ctx['contract.default'].prepopulate!(user_id: current_user.id)
+    end
+
+    def log_errors(ctx, model:, **)
+      contract = ctx['contract.default']
+      ctx[:errors] << contract.errors.full_messages.presence || contract.model.errors.full_messages
+      ctx[:errors] = ctx[:errors].compact.flatten
     end
   end
 end
